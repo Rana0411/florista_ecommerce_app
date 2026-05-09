94fb2c22d7e8788034acd2b58573c1dc26c0b251
Yasser Samir <yasoo976098@gmail.com>
2026-05-10 01:09:09 +0300

    fix(chore): return to origenal

--- lib/core/api_constants/base_urls.dart
+++ lib/core/api_constants/base_urls.dart
@@ -1,6 +1,4 @@
-class BaseUrls {
+ class BaseUrls {
    static const String flowerAppBaseURl = 'https://flower.elevateegy.com/api/v1';
-   static const String flowerSignUP = '$flowerAppBaseURl/auth/signup';
-
 
  }
