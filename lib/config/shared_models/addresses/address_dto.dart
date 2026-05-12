f0cf800fc92edba36064106fa4d0e1e26c0df7e8
Yasser Samir <yasoo976098@gmail.com>
2026-05-12 03:13:49 +0300

    chore(config): add addresses response and dto to shared models

---
+++ lib/config/shared_models/addresses/address_dto.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'address_dto.g.dart';
+
+@JsonSerializable()
+class AddressDto {
+  @JsonKey(name: "street")
+  final String? street;
+  @JsonKey(name: "phone")
+  final String? phone;
+  @JsonKey(name: "city")
+  final String? city;
+  @JsonKey(name: "lat")
+  final String? lat;
+  @JsonKey(name: "long")
+  final String? long;
+  @JsonKey(name: "username")
+  final String? username;
+  @JsonKey(name: "_id")
+  final String? id;
+
+  AddressDto({
+    this.street,
+    this.phone,
+    this.city,
+    this.lat,
+    this.long,
+    this.username,
+    this.id,
+  });
+
+  factory AddressDto.fromJson(Map<String, dynamic> json) =>
+      _$AddressDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AddressDtoToJson(this);
+}
