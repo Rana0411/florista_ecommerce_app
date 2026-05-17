bfaf38937987d026194743ae99fe2688c03c7d9a
Yasser Samir <yasoo976098@gmail.com>
2026-05-18 00:43:57 +0300

    chore(config): update dio module to use auth interceptor

--- lib/config/dio/dio_module.dart
+++ lib/config/dio/dio_module.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:florista_ecommerce_app/config/auth_interceptor/auth_interceptor.dart';
 import 'package:florista_ecommerce_app/core/api_constants/base_urls.dart';
 import 'package:flutter/foundation.dart';
 import 'package:injectable/injectable.dart';
@@ -7,7 +8,7 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 @module
 abstract class DioModule {
   @singleton
-  Dio get dio {
+  Dio dio(AuthInterceptor authInterceptor) {
     final dio = Dio(
       BaseOptions(
         baseUrl: BaseUrls.flowerAppBaseURl,
@@ -16,6 +17,9 @@ abstract class DioModule {
         receiveTimeout: const Duration(seconds: 20),
       ),
     );
+
+    dio.interceptors.add(authInterceptor);
+
     if (kDebugMode) {
       dio.interceptors.add(
         PrettyDioLogger(
