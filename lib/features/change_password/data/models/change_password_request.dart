d31a8f0dea55056ef59a75af0ca3fb6f57f45ac5
Yasser Samir <yasoo976098@gmail.com>
2026-05-21 02:12:57 +0300

    feat(changePassword): implement change password functionality with API client and data sources

---
+++ lib/features/change_password/data/models/change_password_request.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'change_password_request.g.dart';
+
+@JsonSerializable()
+class ChangePasswordRequest {
+  @JsonKey(name: "email")
+  final String? email;
+  @JsonKey(name: "newPassword")
+  final String? newPassword;
+
+  ChangePasswordRequest({this.email, this.newPassword});
+
+  factory ChangePasswordRequest.fromJson(Map<String, dynamic> json) =>
+      _$ChangePasswordRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ChangePasswordRequestToJson(this);
+}
