afc1acfe4a21ea7a07a405f9017d40071b3c02a0
progranahedia <ranamhadiya@gmail.com>
2026-06-03 11:16:15 +0300

    feat: add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:florista_ecommerce_app/config/di/di.dart';
 import 'package:florista_ecommerce_app/config/hive/hive_service.dart';
 import 'package:florista_ecommerce_app/core/router/app_router.dart';
 import 'package:florista_ecommerce_app/core/utils/themes/dark_theme.dart';
 import 'package:florista_ecommerce_app/core/utils/themes/light_theme.dart';
 import 'package:florista_ecommerce_app/generated/l10n.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -13,6 +16,19 @@ void main() async {
 
   final hive = getIt<HiveService>();
   await hive.init();
+  // Initialize Firebase
+  await Firebase.initializeApp();
+
+  // Pass all uncaught asynchronous errors from the framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  // Pass all uncaught Flutter framework errors to Crashlytics
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
 
   runApp(const MyApp());
 }
