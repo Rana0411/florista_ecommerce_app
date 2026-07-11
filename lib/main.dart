2bfbc928774226edac53b9d59c2fc7c86adae128
AbdullahAtef1 <atefabdullah063@gmail.com>
2026-07-11 16:15:41 +0300

    edit track order

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:florista_ecommerce_app/config/di/di.dart';
 import 'package:florista_ecommerce_app/config/hive/hive_service.dart';
 import 'package:florista_ecommerce_app/config/secure_storage/secure_storage_service.dart';
+import 'package:florista_ecommerce_app/core/app_keys/secure_storage_keys.dart';
 import 'package:florista_ecommerce_app/core/router/app_router.dart';
 import 'package:florista_ecommerce_app/core/utils/themes/dark_theme.dart';
 import 'package:florista_ecommerce_app/core/utils/themes/light_theme.dart';
@@ -24,6 +25,12 @@ void main() async {
 
   final hive = getIt<HiveService>();
   await hive.init();
+  await getIt<SecureStorageService>()
+    ..write(
+      key: SecureStorageKeys.token,
+      value:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNmExM2E2YjRhMWUyOThmNTU2MjQxZWVjIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3Nzk2NzI3ODJ9.6nftFCvrGfqq-SvN0ubRG_g0sh7LhMRvPLwGT1N-Yho",
+    );
 
   // NOTE: remove this block entirely once real auth/login flow is wired up.
   // Hardcoded tokens should never ship in source control.
@@ -73,6 +80,7 @@ class MyApp extends StatelessWidget {
               GlobalCupertinoLocalizations.delegate,
             ],
             supportedLocales: S.delegate.supportedLocales,
+
             routerConfig: AppRouter.goRouter,
             theme: TLightTheme.lightTheme,
             darkTheme: TDarkTheme.darkTheme,
